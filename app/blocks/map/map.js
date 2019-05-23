a358e159b23eea8dcd04f01f2b59fb5ee304ac97
Timur Kostenko <ktim8168@mail.ru>
2019-05-23 23:57:34 +0300

    Change map

--- app/blocks/map/map.js
+++ app/blocks/map/map.js
@@ -1,18 +1,25 @@
-/* eslint-disable */
-// Функция ymaps.ready() будет вызвана, когда
-    // загрузятся все компоненты API, а также когда будет готово DOM-дерево.
-    ymaps.ready(init);
-    function init() {
-        // Создание карты.
-      let myMap = new ymaps.Map('map', {
-            // Координаты центра карты.
-            // Порядок по умолчанию: «широта, долгота».
-            // Чтобы не определять координаты центра карты вручную,
-            // воспользуйтесь инструментом Определение координат.
-          center: [55.76, 37.64],
-            // Уровень масштабирования. Допустимые значения:
-            // от 0 (весь мир) до 19.
-          zoom: 7,
-        });
-    }
-/* eslint-enable */
+/* eslint-disable no-unused-vars */
+// http://leafletjs.com
+// Для кластеров использовать: https://github.com/Leaflet/Leaflet.markercluster
+import L from 'leaflet';
+
+const $ = window.$;
+
+export default function maps() {
+  if (!$('#map').length) {
+    return;
+  }
+
+  const map = L.map('map').setView([59.934, 30.335], 13);
+
+  L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+    maxZoom: 18,
+    id: 'mapbox.streets',
+    accessToken: 'pk.eyJ1IjoidGhldmVydmVyeTEiLCJhIjoiY2lzZXdzaXZ4MDBjaTJudm93dDI4MGVrMCJ9.Z8KKk0M_lpDTPB6_JtJBxg',
+  }).addTo(map);
+
+  const marker = L.marker([59.934, 30.335]).addTo(map);
+
+  marker.bindPopup('Кастомный HTML-попап');
+}
+/* eslint-enable no-unused-vars */
